0a669a0a4f895464b4eb950cc5384fb31b6e19e1
kenkatou354 <kenkatou354@gmail.com>
2026-01-13 01:54:35 +0900

    [add]playerSatatusSaveを追加

---
+++ Assets/Scripts/Develop/Save/PlayerStatusSaveData.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Develop.Save
+{
+    /// <summary>
+    /// ゲームのセーブデータを管理するクラス。
+    /// </summary>
+    public class PlayerStatusSaveData 
+    {
+        /// <summary>
+        /// セーブデータを生成する。
+        /// </summary>
+        /// <param name="money"> 所持金 </param>   
+        public PlayerStatusSaveData(int money)
+        {
+            Money = money;
+        }
+
+        public int Money { get; private set; }
+    }
+}
