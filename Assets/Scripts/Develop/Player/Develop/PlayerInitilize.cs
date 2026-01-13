9725b7ce53408377e867c78c0c520a9310453ede
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-01-13 23:14:03 +0900

    [update]init

--- Assets/Scripts/Develop/Player/Develop/PlayerInitilize.cs
+++ Assets/Scripts/Develop/Player/Develop/PlayerInitilize.cs
@@ -3,5 +3,20 @@ using UnityEngine;
 
 public class PlayerInitilize : MonoBehaviour
 {
-    
+    [SerializeField] private PlayerPresenter _playerPresenter;
+    [SerializeField] private PlayerViwer _playerViwer;
+    [SerializeField] private int _initialMoney = 1000;
+
+    private PlayerData _playerData;
+
+    private void Awake()
+    {
+        _playerData = new PlayerData(_initialMoney);
+        _playerPresenter.Init(_playerData, _playerViwer);
+    }
+
+    private void OnDestroy()
+    {
+        _playerData?.OnDestroy();
+    }
 }
--- Assets/Scripts/Develop/Player/PlayerPresenter.cs
+++ Assets/Scripts/Develop/Player/PlayerPresenter.cs
@@ -3,31 +3,31 @@ using UniRx;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlayerPresenter : MonoBehaviour,IPointerClickHandler
+public class PlayerPresenter : MonoBehaviour, IPointerClickHandler
 {
     private CompositeDisposable _disposables;
     private PlayerData _playerData;
     private PlayerViwer _playerViwer;
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        _playerData.AddMoney(1);
-    }
-
-    private void Start()
+    public void Init(PlayerData playerData, PlayerViwer playerViwer)
     {
         _disposables = new CompositeDisposable();
-        _playerData = new PlayerData(0);
-        _playerViwer = GetComponent<PlayerViwer>();
+        _playerData = playerData;
+        _playerViwer = playerViwer;
 
         _playerData.Money
             .Subscribe(money => _playerViwer.SetCount(money))
             .AddTo(_disposables);
     }
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        _playerData?.AddMoney(1);
+    }
+
     private void OnDestroy()
     {
-        _disposables.Dispose();
+        _disposables?.Dispose();
         _playerData?.OnDestroy();
     }
 }
