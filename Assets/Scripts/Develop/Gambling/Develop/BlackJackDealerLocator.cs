46eb62d4b7d6ab54fc0c882292d8f927e283fe94
kenkatou354 <kenkatou354@gmail.com>
2026-01-14 07:08:36 +0900

    [add] ロケータクラスの追加

---
+++ Assets/Scripts/Develop/Gambling/Develop/BlackJackDealerLocator.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+namespace Develop.Gambling.Develop
+{
+    /// <summary>
+    /// ブラックジャックのディーラーを取得するロケータークラス。
+    /// </summary>
+    public static class BlackJackDealerLocator
+    {
+        private static BlackJackDealer _dealer;
+
+        /// <summary>
+        ///     BlackJackDealer を取得する。
+        ///     既に存在する場合はそれを返し、存在しない場合は新しく生成して返す。
+        /// </summary>
+        /// <retruns> ディーラー </retruns>
+        public static BlackJackDealer GetDealer()
+        {
+            if (_dealer != null)
+            {
+                return _dealer;
+            }
+
+            GameObject dealerObject = new GameObject("BlackJackDealer");
+            _dealer = dealerObject.AddComponent<BlackJackDealer>();
+
+            return _dealer;
+        }
+    }
+}
