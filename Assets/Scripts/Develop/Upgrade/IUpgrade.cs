3f50b86abd180a3bdeaae9e0530417920d742aae
ayato0913a <ayato0913a@gmail.com>
2026-01-15 02:22:27 +0900

    [ feat ] upgradeのインターフェースを作成

--- Assets/Scripts/Develop/Upgrade/IUpgrade.cs
+++ Assets/Scripts/Develop/Upgrade/IUpgrade.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 namespace Develop.Upgrade
 {
+    /// <summary>
+    /// アップグレードのインターフェース。 
+    /// </summary>
     public interface IUpgrade
     {
-
+        /// <summary> アップグレードの名前。</summary>
+        string Name { get; }
+        /// <summary> アップグレードのレベル。</summary>
+        int Level { get; }
+        /// <summary> アップグレードの最大レベル。</summary>
+        int MaxLevel { get; }
+        /// <summary> アップグレードのコスト。</summary>
+        int Cost { get; }
+        /// <summary> アップグレードを適用する。 </summary>
+        void ApplyUpgrade();
     }
 }
