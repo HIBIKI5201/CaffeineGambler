af2c40febe274bec43476dcc856948573f1acc0b
ayato0913a <ayato0913a@gmail.com>
2026-01-14 20:05:36 +0900

    [ feat ] Save用のGameManagerを追加

--- Assets/Scripts/Develop/Save/GameManager.cs
+++ Assets/Scripts/Develop/Save/GameManager.cs
@@ -1,6 +1,58 @@
+using Cysharp.Threading.Tasks;
+using Develop.Player;
+using UniRx;
 using UnityEngine;
 
-public class GameManager
+namespace Develop.Save
 {
-    
+    /// <summary> セーブお試し用クラス。 </summary>
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+
+        private PlayerDataHandler _playerDataHandler;
+
+        public PlayerData PlayerData => _playerDataHandler?.PlayerData;
+
+        [SerializeField] private PlayerViewer _playerViewer;
+
+        private CompositeDisposable _disposables;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            _disposables = new CompositeDisposable();
+            _playerDataHandler = new PlayerDataHandler();
+            _playerDataHandler.Load();
+        }
+
+        private void OnApplicationQuit()
+        {
+            _playerDataHandler?.Save();
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                _playerDataHandler?.OnDestroy();
+            }
+        }
+
+        private void Update()
+        {
+            PlayerData?.AddMoney(1);
+            PlayerData.Money.Subscribe(money => _playerViewer.SetCount(money)).AddTo(_disposables);
+        }
+    }
 }
