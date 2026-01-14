960bc48979a595f1d2f4c55a3eaff37e4756f4ec
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-01-14 15:52:51 +0900

    [add]vardviewer

---
+++ Assets/Scripts/Develop/Poker/CardViewer.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CardViewer : MonoBehaviour
+{
+    [SerializeField] private List<TextMeshProUGUI> _cardTexts;
+
+    public void SetCards(IReadOnlyList<Card> cards)
+    {
+        for (var i = 0; i < _cardTexts.Count; i++)
+        {
+            if (cards != null && i < cards.Count)
+            {
+                _cardTexts[i].text = FormatCard(cards[i]);
+            }
+            else
+            {
+                _cardTexts[i].text = string.Empty;
+            }
+        }
+    }
+
+    private static string FormatCard(Card card)
+    {
+        if (card.IsJoker)
+        {
+            return "Joker";
+        }
+
+        var rankLabel = card.Rank switch
+        {
+            11 => "J",
+            12 => "Q",
+            13 => "K",
+            14 => "A",
+            _ => card.Rank.ToString()
+        };
+
+        return $"{card.Suit} {rankLabel}";
+    }
+}
--- Assets/Scripts/Develop/Poker/CatdViewer.cs
+++ Assets/Scripts/Develop/Poker/CatdViewer.cs
@@ -1,12 +1,23 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class CatdViewer : MonoBehaviour
 {
-    [SerializeField] private TextMeshPro _cardType;
+    [SerializeField] private List<TextMeshProUGUI> _cardTexts;
 
     public void SetCardType(string cardType)
     {
-        _cardType.text = cardType;
+        for(int i=0; i < _cardTexts.Count; i++)
+        {
+            if (i < cardType.Length)
+            {
+                _cardTexts[i].text = cardType[i].ToString();
+            }
+            else
+            {
+                _cardTexts[i].text = string.Empty;
+            }
+        }
     }
 }
