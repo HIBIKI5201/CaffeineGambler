8e1890c0491c4a6bc992c44e65d43e21403ae25d
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-01-14 16:10:08 +0900

    [update]cardpresenter

--- Assets/Scripts/Develop/Poker/CardPresenter.cs
+++ Assets/Scripts/Develop/Poker/CardPresenter.cs
@@ -1,16 +1,32 @@
+using TMPro;
 using UnityEngine;
 
 public class CardPresenter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private PokerGameManager _gameManager;
+    [SerializeField] private CardViewer _cardViewer;
+    [SerializeField] private TextMeshProUGUI _rankLabel;
+
+    private void Start()
     {
-        
+        RefreshView();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDealButton()
     {
-        
+        _gameManager.DealInitialHand();
+        RefreshView();
+    }
+
+    public void OnEvaluateButton()
+    {
+        var rank = _gameManager.EvaluateCurrentHand();
+        _rankLabel?.SetText(rank.ToString());
+    }
+
+    public void RefreshView()
+    {
+        _cardViewer?.SetCards(_gameManager.CurrentHand);
+        _rankLabel?.SetText(_gameManager.EvaluateCurrentHand().ToString());
     }
 }
