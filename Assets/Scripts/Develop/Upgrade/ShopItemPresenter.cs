4e5ed771326b3c0af165bd21b19353e73696f3ab
ayato0913a <ayato0913a@gmail.com>
2026-01-15 02:53:22 +0900

    [ add ] クラス作成

---
+++ Assets/Scripts/Develop/Upgrade/ShopItemPresenter.cs
@@ -0,0 +1,12 @@
+using UniRx;
+using UnityEngine;
+
+namespace Develop.Upgrade
+{
+    /// <summary>
+    /// ショップアイテムのプレゼンター。
+    /// </summary>
+    public class ShopItemPresenter : MonoBehaviour
+    {
+    }
+}
