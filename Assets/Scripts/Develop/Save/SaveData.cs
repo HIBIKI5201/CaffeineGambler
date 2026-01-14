3f28e56aba4f59b0f85dbf94dd81c5df9c392b77
ayato0913a <ayato0913a@gmail.com>
2026-01-14 17:17:39 +0900

    [ fix ] summary追加

--- Assets/Scripts/Develop/Save/SaveData.cs
+++ Assets/Scripts/Develop/Save/SaveData.cs
@@ -2,25 +2,33 @@ using UnityEngine;
 
 namespace Develop.Save
 {
+    /// <summary>
+    ///     プレイヤーのデータを保存・読み込みするクラス。
+    ///     所持金や強化レベルなどを保存・読み込みする。
+    /// </summary>
     public static class SaveData
     {
+         /// <summary> Int値を保存する</summary>
         public static void SaveInt(string key, int value)
         {
             PlayerPrefs.SetInt(key, value);
             PlayerPrefs.Save();
         }
 
+        /// <summary> Int値を読み込む </summary>
         public static int LoadInt(string key, int defaultValue = 0)
         {
             return PlayerPrefs.GetInt(key, defaultValue);
         }
 
+        /// <summary> 文字列を保存する </summary>
         public static void SaveString(string key, string value)
         {
             PlayerPrefs.SetString(key, value);
             PlayerPrefs.Save();
         }
 
+        /// <summary> 文字列を読み込む </summary>
         public static string LoadString(string key, string defaultValue = "")
         {
             return PlayerPrefs.GetString(key, defaultValue);
