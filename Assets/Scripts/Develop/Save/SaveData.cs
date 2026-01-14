e2e12c8efb04f7dad3848c07915cbbb8b6d3cfd3
ayato0913a <ayato0913a@gmail.com>
2026-01-14 16:25:39 +0900

    [ feat ] 数値と文字列を保存できる機能を追加

--- Assets/Scripts/Develop/Save/SaveData.cs
+++ Assets/Scripts/Develop/Save/SaveData.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
 
-public class SaveData
+namespace Develop.Save
 {
-    
+    public static class SaveData
+    {
+        public static void SaveInt(string key, int value)
+        {
+            PlayerPrefs.SetInt(key, value);
+            PlayerPrefs.Save();
+        }
+
+        public static int LoadInt(string key, int defaultValue = 0)
+        {
+            return PlayerPrefs.GetInt(key, defaultValue);
+        }
+
+        public static void SaveString(string key, string value)
+        {
+            PlayerPrefs.SetString(key, value);
+            PlayerPrefs.Save();
+        }
+
+        public static string LoadString(string key, string defaultValue = "")
+        {
+            return PlayerPrefs.GetString(key, defaultValue);
+        }
+    }
 }
