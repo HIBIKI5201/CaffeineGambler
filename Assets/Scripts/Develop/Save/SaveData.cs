31f25f41de9629275f6e50269b8e7ce6c4b06813
ayato0913a <ayato0913a@gmail.com>
2026-01-14 17:54:06 +0900

    [ fix ] 修正

--- Assets/Scripts/Develop/Save/SaveData.cs
+++ Assets/Scripts/Develop/Save/SaveData.cs
@@ -8,14 +8,14 @@ namespace Develop.Save
     /// </summary>
     public static class SaveData
     {
-         /// <summary> Int値を保存する</summary>
+         /// <summary> 整数を保存する</summary>
         public static void SaveInt(string key, int value)
         {
             PlayerPrefs.SetInt(key, value);
             PlayerPrefs.Save();
         }
 
-        /// <summary> Int値を読み込む </summary>
+        /// <summary> 整数を読み込む </summary>
         public static int LoadInt(string key, int defaultValue = 0)
         {
             return PlayerPrefs.GetInt(key, defaultValue);
