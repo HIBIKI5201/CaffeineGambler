49d81b6613894aecac20b64f7c9bff55706c8b22
ayato0913a <ayato0913a@gmail.com>
2026-01-15 11:00:30 +0900

    [ feat ] ボタンの生成処理とか作った

--- Assets/Scripts/Develop/Upgrade/ShopManager.cs
+++ Assets/Scripts/Develop/Upgrade/ShopManager.cs
@@ -1,16 +1,47 @@
+using System;
+using UniRx;
+using Develop.Player;
 using UnityEngine;
 
-public class ShopManager : MonoBehaviour
+namespace Develop.Upgrade
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ShopManager : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Transform _contentRoot;
+        [SerializeField] private ShopItemView _shopItemViewPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private UpgradeShopService _upgradeShopService;
+
+        public void Init(PlayerData playerData, IUpgrade[] upgrades)
+        {
+            _upgradeShopService = new UpgradeShopService(playerData);
+
+            foreach (var upgrade in upgrades)
+            {
+                CreateItem(upgrade);
+            }
+        }
+
+        private void CreateItem(IUpgrade upgrade)
+        {
+            ShopItemView shopItemView = Instantiate(_shopItemViewPrefab, _contentRoot);
+            ShopItemPresenter presenter = shopItemView.GetComponent<ShopItemPresenter>();
+
+            shopItemView.Set(upgrade);
+            presenter.Init(shopItemView);
+
+            presenter.OnBuyClicked
+                .Subscribe(_ =>
+                {
+                    if (_upgradeShopService.TryPurchaseUpgrade(upgrade))
+                    {
+                        shopItemView.Set(upgrade);
+                    }
+                })
+                .AddTo(presenter);
+        }
     }
 }
