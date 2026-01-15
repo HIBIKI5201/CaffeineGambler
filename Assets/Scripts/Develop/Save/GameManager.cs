ebca68f577ad86c252163f9da236cd512f11774c
ayato0913a <ayato0913a@gmail.com>
2026-01-16 02:11:35 +0900

    [ fix ] 使わない

--- Assets/Scripts/Develop/Save/GameManager.cs
+++ Assets/Scripts/Develop/Save/GameManager.cs
@@ -12,47 +12,45 @@ namespace Develop.Save
 
         private PlayerDataHandler _playerDataHandler;
 
-        public PlayerData PlayerData => _playerDataHandler?.PlayerData;
-
         [SerializeField] private PlayerViewer _playerViewer;
 
         private CompositeDisposable _disposables;
 
         private void Awake()
         {
-            if (Instance == null)
-            {
-                Instance = this;
-                DontDestroyOnLoad(gameObject);
-            }
-            else
-            {
-                Destroy(gameObject);
-                return;
-            }
+            //if (Instance == null)
+            //{
+            //    Instance = this;
+            //    DontDestroyOnLoad(gameObject);
+            //}
+            //else
+            //{
+            //    Destroy(gameObject);
+            //    return;
+            //}
 
-            _disposables = new CompositeDisposable();
-            _playerDataHandler = new PlayerDataHandler();
-            _playerDataHandler.Load();
-            PlayerData.Money.Subscribe(money => _playerViewer.SetCount(money)).AddTo(_disposables);
+            //_disposables = new CompositeDisposable();
+            //_playerDataHandler = new PlayerDataHandler();
+            //_playerDataHandler.Load();
+            //PlayerData.Money.Subscribe(money => _playerViewer.SetCount(money)).AddTo(_disposables);
         }
 
-        private void OnApplicationQuit()
-        {
-            _playerDataHandler?.Save();
-        }
+        //private void OnApplicationQuit()
+        //{
+        //    _playerDataHandler?.Save();
+        //}
 
-        private void OnDestroy()
-        {
-            if (Instance == this)
-            {
-                _playerDataHandler?.OnDestroy();
-            }
-        }
+        //private void OnDestroy()
+        //{
+        //    if (Instance == this)
+        //    {
+        //        _playerDataHandler?.OnDestroy();
+        //    }
+        //}
 
-        private void Update()
-        {
-            PlayerData?.AddMoney(1);
-        }
+        //private void Update()
+        //{
+        //    PlayerData?.AddMoney(1);
+        //}
     }
 }
