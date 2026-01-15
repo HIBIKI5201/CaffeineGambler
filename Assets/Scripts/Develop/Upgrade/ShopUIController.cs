4e508322b395dfe0a4494048751d8b854d10b370
ayato0913a <ayato0913a@gmail.com>
2026-01-15 10:58:34 +0900

    [ feat ] 入力からの移動などを実装

--- Assets/Scripts/Develop/Upgrade/ShopUIController.cs
+++ Assets/Scripts/Develop/Upgrade/ShopUIController.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using DG.Tweening;
 
-public class ShopUIController : MonoBehaviour
+namespace Develop.Upgrade
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class ShopUIController : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private RectTransform _shopPanel;
+        [SerializeField] private float _animationDuration;
+        [SerializeField] private Ease _animationEase;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private float _width;
+        private bool _isOpen = false;
+        private Tween _currentTween;
+
+        private void ToggleShopPanel()
+        {
+            _currentTween?.Kill();
+            if (_isOpen)
+            {
+                _currentTween = _shopPanel.DOAnchorPosX(-_width, _animationDuration).SetEase(_animationEase);
+            }
+            else
+            {
+                _currentTween = _shopPanel.DOAnchorPosX(0, _animationDuration).SetEase(_animationEase);
+            }
+            _isOpen = !_isOpen;
+        }
+
+        private void Start()
+        {
+            _width = _shopPanel.rect.width;
+
+            _shopPanel.anchoredPosition = new Vector2(-_width, 0);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                ToggleShopPanel();
+            }
+        }
     }
 }
