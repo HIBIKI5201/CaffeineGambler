fbfc896bad38bfc836e71cd28f3d5756d63f0928
ayato0913a <ayato0913a@gmail.com>
2026-01-17 14:49:15 +0900

    [ fix ] PlayerDataSaveに合わせて変更

--- Assets/Scripts/Develop/Save/PlayerDataHandler.cs
+++ Assets/Scripts/Develop/Save/PlayerDataHandler.cs
@@ -9,25 +9,22 @@ namespace Develop.Save
     /// </summary>
     public class PlayerDataHandler
     {
-        public void LoadAndApply(PlayerData target)
+        public void LoadAndApply(PlayerData player)
         {
             // Keyは PlayerData の型名、実体は PlayerDataSave
             var loadedSave = SaveData.LoadJson(
                 keyType: typeof(PlayerData),
-                defaultValue: new PlayerDataSave { Money = 1000 }
+                defaultValue: default(PlayerDataSave)
             );
 
             if (loadedSave == null)
                 return;
 
-            target.Money.Value = loadedSave.Money;
+            loadedSave.Convert(player);
         }
         public void Save(PlayerData playerData)
         {
-            var save = new PlayerDataSave
-            {
-                Money = playerData.Money.Value
-            };
+            var save = new PlayerDataSave(playerData);
 
             // KeyはPlayerDataの型名
             SaveData.SaveJson(typeof(PlayerData), save);
