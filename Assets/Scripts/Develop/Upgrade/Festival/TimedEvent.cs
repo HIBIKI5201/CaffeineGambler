6805370fcace6a03717efcf8343821f9cee3629e
kenkatou354 <kenkatou354@gmail.com>
2026-01-17 19:11:34 +0900

    [add] ドメインを追加

--- Assets/Scripts/Develop/Upgrade/Festival/TimedEvent.cs
+++ Assets/Scripts/Develop/Upgrade/Festival/TimedEvent.cs
@@ -1,26 +1,53 @@
+using System;
+
 namespace Develop.Upgrade.Festival
 {
      public class TimedEvent
     {
 
-        public TimedEvent(FakeClock clock, FakeRandom random)
+        public TimedEvent(FakeClock clock, FakeRandom random,int eventTime)
         {
             this._clock = clock;
             this._random = random;
-
+            this._eventTime = eventTime;
             _nextStartTime = _clock.Now + _random.Range(30, 90);
         }
-        public bool IsActiv { get; private set; }
+        public bool IsActive { get; private set; }
 
         public void Update()
         {
-            if(!IsActiv && _clock.Now >= _nextStartTime)
-                IsActiv = true;
+            if (!IsActive && _clock.Now >= _nextStartTime)
+            {
+                EventStart();
+            }
+
+            if (IsActive && _clock.Now >= _endTime)
+            {
+                EventEnd();
+            }
+        }
+
+        private void EventStart()
+        {
+            IsActive = true;
+            _endTime = _clock.Now + _eventTime;
+        }
+        private void EventEnd()
+        {
+            IsActive = false;
+            ScheduleNextStart();
+        }
+
+        private void ScheduleNextStart()
+        {
+            _nextStartTime = _clock.Now + _random.Range(30, 90);
         }
 
         private FakeClock _clock;
         private FakeRandom _random;
         private int _nextStartTime;
+        private int _eventTime;
+        private int _endTime;
 
     }
 }
