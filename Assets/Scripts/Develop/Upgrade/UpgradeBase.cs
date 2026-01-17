34caa9ef003223f0a92eb832a56c886dfeaa7eff
ayato0913a <ayato0913a@gmail.com>
2026-01-17 15:46:53 +0900

    [ feat ] 基底に共通処理をまとめる

--- Assets/Scripts/Develop/Upgrade/UpgradeBase.cs
+++ Assets/Scripts/Develop/Upgrade/UpgradeBase.cs
@@ -1,6 +1,26 @@
-using UnityEngine;
-
-public class UpgradeBase
+namespace Develop.Upgrade
 {
-    
+    /// <summary>
+    /// アップグレードの基底クラス。
+    /// </summary>
+    public abstract class UpgradeBase : IUpgrade
+    {
+        public abstract string Name { get; }
+
+        public int Level { get; private set; }
+
+        public abstract int MaxLevel { get; }
+
+        public bool CanLevelUp => Level < MaxLevel;
+
+        public void ApplyUpgrade()
+        {
+            if (CanLevelUp)
+            {
+                Level++;
+            }
+        }
+
+        public abstract int GetCost();
+    }
 }
