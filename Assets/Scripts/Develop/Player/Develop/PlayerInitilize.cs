d3d6392d08b39d346c53d3f878d4f0baf46c9e58
ayato0913a <ayato0913a@gmail.com>
2026-01-18 12:14:36 +0900

    [ deleate ] 初期化呼ばないように

--- Assets/Scripts/Develop/Player/Develop/PlayerInitilize.cs
+++ Assets/Scripts/Develop/Player/Develop/PlayerInitilize.cs
@@ -12,7 +12,7 @@ public class PlayerInitilize : MonoBehaviour
     private void Awake()
     {
         _playerData = new PlayerData(_initialMoney);
-        _playerPresenter.Init(_playerData);
+        //_playerPresenter.Init(_playerData);
     }
 
     private void OnDestroy()
