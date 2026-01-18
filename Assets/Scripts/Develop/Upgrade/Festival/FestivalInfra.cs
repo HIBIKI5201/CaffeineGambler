956edfff63f3bfeb4a8b5aacf5120197c9599bf0
kenkatou354 <kenkatou354@gmail.com>
2026-01-18 11:45:25 +0900

    [add] レベル処理を追加

--- Assets/Scripts/Develop/Upgrade/Festival/FestivalInfra.cs
+++ Assets/Scripts/Develop/Upgrade/Festival/FestivalInfra.cs
@@ -1,48 +1,58 @@
 using Develop.Upgrade.Festival;
 using Domain;
 using UnityEngine;
+using System.Collections.Generic;
 
-public class FestivalInfra
+/// <summary>
+/// フェスティバル機能のUnityインフラ層。
+/// </summary>
+public class FestivalInfra : MonoBehaviour
 {
     [Header("Event Settings")]
-    [SerializeField] private int _eventDuration = 5; // イベント時間（秒）
-    [SerializeField] private int _threshold = 3;     // ボーナス閾値
-    [SerializeField] private int _multiplier = 2;    // ボーナス倍率
+    [SerializeField] private int _eventDuration = 5;
+
+    [Header("Level Settings")]
+    [SerializeField] private RewardRankSO _levelData;
+    [SerializeField] private int _currentLevel = 1;
 
     private TimedEvent _timeEvent;
     private EventReward _eventReward;
-
 
     private void Start()
     {
         IClock clock = new SystemClock();
         IRandom random = new SystemRandom();
 
-        // ドメインオブジェクトの生成
+        // オブジェクトの構築
         _timeEvent = new TimedEvent(clock, random, _eventDuration);
-        _eventReward = new EventReward(_timeEvent, _threshold, _multiplier);
+        _eventReward = new EventReward(_timeEvent);
 
+        // イベント購読によるライフサイクル管理
         _timeEvent.OnStarted += _eventReward.OnEventStarted;
-        _timeEvent.OnEnded += _eventReward.OnEventEnded;
+        _timeEvent.OnEnded += HandleEventEnded;
+
         Debug.Log("Festival System Init Complete");
-    }
-    private void Dispose()
-    {
-        _timeEvent.OnStarted -= _eventReward.OnEventStarted;
-        _timeEvent.OnEnded -= _eventReward.OnEventEnded;
     }
 
     private void Update()
     {
+        // 時間の監視
         _timeEvent.Update();
 
+        // 採取入力の監視
         if (Input.GetMouseButtonDown(0))
         {
             _eventReward.OnHarvest();
             _eventReward.AddCoffeeBeans(1);
-
         }
+    }
 
-
+    /// <summary>
+    /// イベント開始時の設定およびリセット処理。
+    /// </summary>
+    private void HandleEventEnded()
+    {
+        _eventReward.ConfigureRanks(_currentLevel, _levelData);
+        _eventReward.OnEventEnded();   
     }
 }
