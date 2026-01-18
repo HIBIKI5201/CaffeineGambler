58d3113e141543e12f89fc6253476a02d7bb5b3b
ayato0913a <ayato0913a@gmail.com>
2026-01-18 19:04:43 +0900

    [ feat ] 作業進捗

--- Assets/Scripts/Develop/Upgrade/GameUpgrades/FestivalUpgrade.cs
+++ Assets/Scripts/Develop/Upgrade/GameUpgrades/FestivalUpgrade.cs
@@ -1,6 +1,14 @@
+using Develop.Upgrade;
 using UnityEngine;
 
-public class FestivalUpgrade
+public class FestivalUpgrade : UpgradeBase
 {
-    
+    public override string Name => "収穫祭";
+
+    public override int MaxLevel => 3;
+
+    public override int GetCost()
+    {
+        throw new System.NotImplementedException();
+    }
 }
