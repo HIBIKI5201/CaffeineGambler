81c385f73553297141b81bd5b39761f4c17061e5
kenkatou354 <kenkatou354@gmail.com>
2026-01-18 11:44:42 +0900

    [add] 強化レベルステータスをもつ

---
+++ Assets/Scripts/Develop/Upgrade/Festival/RewardRank_1.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+namespace Develop.Upgrade.Festival
+{
+    public class RewardRank : MonoBehaviour
+    {
+        /// <summary>
+        /// 強化レベル。
+        /// </summary>
+        public int Level;
+
+        /// <summary>
+        /// 報酬獲得に必要なカウンターの閾値（m）。
+        /// </summary>
+        public int Threshold;
+
+        /// <summary>
+        /// 適用される報酬倍率（n）。
+        /// </summary>
+        public int Multiplier;
+    }
+}
