81377a3ccb3f1103e0c9b1a9395b3dec4a688ffe
ayato0913a <ayato0913a@gmail.com>
2026-01-18 12:04:40 +0900

    [ feat ] 強化の初期化クラス作成

--- Assets/Scripts/Runtime/Upgrade/UpgradeInitializer.cs
+++ Assets/Scripts/Runtime/Upgrade/UpgradeInitializer.cs
@@ -1,16 +1,28 @@
+using Develop.Player;
+using Develop.Upgrade;
 using UnityEngine;
+using System.Collections.Generic;
 
-public class UpgradeInitializer : MonoBehaviour
+namespace Runtime.Upgrade
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// Upgrade 系の初期化だけを担当するクラス
+    /// </summary>
+    public class UpgradeInitializer : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private ShopManager _shopManager;
+        [SerializeField] private AutoProductionRunner _autoProductionRunner;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        /// <summary>
+        /// Upgrade 系の初期化をまとめて行う
+        /// </summary>
+        public void Init(PlayerData playerData, List<IUpgrade> upgrades)
+        {
+            // Shop に渡す（購入処理）
+            _shopManager.Init(playerData, upgrades.ToArray());
+
+            // 自動生産に渡す
+            _autoProductionRunner.Init(playerData, upgrades);
+        }
     }
 }
